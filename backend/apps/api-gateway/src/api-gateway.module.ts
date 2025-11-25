2d7e9aff3d72c28eb4053d12941f2b903452089a
shimri <shimri86@gmail.com>
2025-11-25 10:30:21 +0200

    Add database configuration and implement orders module in API gateway

--- backend/apps/api-gateway/src/api-gateway.module.ts
+++ backend/apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    OrdersModule,
+  ],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
 })
